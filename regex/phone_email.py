c7ad113e92abcdcbf6e987725c4f5014afbbfdb4
Waseem Ahmed <alphawaseem@gmail.com>
2017-05-03 14:12:41 +0530

    find phone and emails

--- regex/phone_email.py
+++ regex/phone_email.py
@@ -1,7 +1,9 @@
-#! python
+#!/home/peace/anaconda3/bin/python
 import pyperclip
 import re
 
+
+# Regex object for phone number
 phoneRegex = re.compile(u'''(
     (\d{3}|\(\d{3}\))?
     (\s|-|\.)?
@@ -10,9 +12,32 @@ phoneRegex = re.compile(u'''(
     (\d{4})  
 )''', re.VERBOSE)
 
+# Regex Object for email address
 emailRegex = re.compile(u'''(
     ([a-zA-Z0-9._%+-]+)
     @
     ([a-zA-Z0-9.-]+)
     (\.[a-zA-Z]{2,4})
 )''', re.VERBOSE)
+
+# Get String from clipboard
+text = str(pyperclip.paste())
+matches = []
+if text:
+    # Perform search
+    print('Performing search.....')
+
+    # Find phone numbers
+    for groups in phoneRegex.findall(text):
+        phoneNum = '-'.join([groups[1], groups[3], groups[5]])
+        print(phoneNum)
+        matches.append(phoneNum)
+
+    # Find email address
+    for groups in emailRegex.findall(text):
+        matches.append(groups[0])
+        print(groups[0])
+
+
+else:
+    print('Please copy something to clipboard and try again.')
